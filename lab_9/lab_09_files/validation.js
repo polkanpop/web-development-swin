e46fab3de60d07a3d9abdb3e8adfea3952283bf9
polkanpop <kanniepop@gmail.com>
2023-10-30 13:44:53 +0700

    add lab_9 files

---
+++ lab_9/lab_09_files/validation.js
@@ -0,0 +1,47 @@
+/* write functions that define the action for each event */
+function validate() {
+	var sid = document.getElementById("sid").value;
+	var pwd1 = document.getElementById("pwd1").value;
+	var pwd2 = document.getElementById("pwd2").value;
+	var uname = document.getElementById("uname").value;
+	/* __(8)__ */
+
+	var errMsg = "";								/* stores the error message */
+	var result = true;							/* assumes no errors */
+	var pattern = /^[a-zA-Z ]+$/;		/* regular expression for letters and spaces only */
+
+	/* Rule 1, check if all required inputs have value */
+	/* __(9)__ */
+	
+	/* Rule 2, check if the user ID contains an @ symbol  */
+	/* __(10)__ */
+	
+	/* Rule 3, check if password and retype password are the same */
+	if (pwd1 != pwd2) {
+		errMsg += "Passwords do not match.\n";
+	}
+	
+	/* Rule 4, check if user name contains only letters and spaces */
+	if (! uname.match (pattern)) {
+		errMsg += "User name contains symbols.\n";
+	}
+
+	/* Display error message any error(s) is/are detected */
+	if (errMsg != "") {
+		alert (errMsg);
+		result = false;
+	} 
+	return result;
+}
+
+/* link HTML elements to corresponding event function */
+function init () {
+	/* link the variables to the HTML elements */
+  var regForm = 	document.getElementById("regform");
+
+	/* assigns functions to corresponding events */
+  regForm.onsubmit = validate;
+}
+
+/* execute the initialisation function once the window*/
+window.onload = init;
