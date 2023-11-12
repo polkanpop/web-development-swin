a5d5f7f6b9062cb56124ddf4bb1f757207e5ba70
Kon <kanniepop@gmail.com>
2023-11-13 00:45:44 +0700

    finished?

--- labs/lab_10/lab_10_files/validation.js
+++ labs/lab_10/lab_10_files/validation.js
@@ -1,11 +1,20 @@
+function toggle(){
+	$(this).parent().next().slideToggle();
+	if ($(this).html() == "[-]"){   
+  		$(this).html("[+]"); 
+ 	} else {         
+  		$(this).html("[-]"); 
+ 	}  
+}
+
 /* function validate()will validate form data */
 function validate() {
-	var sid = document.getElementById("sid").value;
-	var pwd1 = document.getElementById("pwd1").value;
-	var pwd2 = document.getElementById("pwd2").value;
-	var uname = document.getElementById("uname").value;
-	var genm = document.getElementById("genm").checked;
-	var genf = document.getElementById("genf").checked;
+	var sid = $("#sid").val();
+	var pwd1 = $("#pwd1").val();
+	var pwd2 = $("#pwd2").val();
+	var uname = $("#uname").val();
+	var genm = $("#genm").prop("checked");
+	var genf = $("#genf").prop("checked");
 
 	var errMsg = "";								/* create variable to store the error message */
 	var result = true;								/* assumes no errors */
@@ -13,55 +22,65 @@ function validate() {
 
 	/* Rule 1, check if all required date are entered */
 	if (sid == "") {								//check whether User ID is empty
-		errMsg += "User ID cannot be empty.\n";
+		errMsg += "<li>User ID cannot be empty.</li>";
 	}
 	if (pwd1 == "") {								//check whether Password is empty
-		errMsg += "Password cannot be empty.\n";
+		errMsg += "<li>Password cannot be empty.</li>";
 	}
 	if (pwd2 == "") {								//check whether re-typed Password is empty
-		errMsg += "Retype password cannot be empty.\n";
+		errMsg += "<li>Retype password cannot be empty.</li>";
 	}
 	if (uname == "") {								//check whether User Name is empty
-		errMsg += "User name cannot be empty.\n";
+		errMsg += "<li>User name cannot be empty.</li>";
 	}
-	if ((genm == "")&&(genf == "")) {				//check whether gender is selected
-		errMsg += "A gender must be selected.\n";
+	if ((!genm)&&(!genf)) {				//check whether gender is selected
+		errMsg += "<li>A gender must be selected.</li>";
 	}
 	
 	/* Rule 2, check if the user ID contains an @ symbol */
 	if (sid.indexOf('@') == 0 ) {
-		errMsg += "User ID cannot start with an @ symbol.\n";
+		errMsg += "<li>User ID cannot start with an @ symbol.</li>";
 	}
 	if (sid.indexOf('@') < 0 ) {
-		errMsg += "User ID must contain an @ symbol.\n";
+		errMsg += "<li>User ID must contain an @ symbol.</li>";
 	}
 	
 	/* Rule 3, check if password and retype password are the same */
 	if (pwd1 != pwd2) {
-		errMsg += "Passwords do not match.\n";
+		errMsg += "<li>Passwords do not match.</li>";
 	}
 	
 	/* Rule 4, check if user name contains only letters and spaces */
-	if (! uname.match (pattern)) {
-		errMsg += "User name contains symbols.\n";
+	if (!uname.match(pattern)) {
+		errMsg += "<li>User name contains symbols.</li>";
 	}
 
-	/* Display error message any error(s) is/are detected */
-	if (errMsg != "") {
-		alert (errMsg);
-		result = false;
-	} 
+	/* Display error message if any error(s) is/are detected */ 
+ 	if (errMsg != "") { 
+  		errMsg =   "<div id='scrnOverlay'></div>" + "<section id='errWin' class='window'><ul>" + errMsg + "</ul><a href='#' id='errBtn' class='button'>Close</a></section>"; 
+		  var numOfItems = ((errMsg.match(/<li>/g)).length) + 6;    //8.5 
+   
+		$("body").after(errMsg);      //8.6 
+		$("#scrnOverlay").css('visibility', 'visible');   //8.7 
+		$("#errWin").css('height', numOfItems.toString() + 'em'); //8.8 
+		$("#errWin").css('margin-top', (numOfItems/-2).toString() + 'em'); //8.8 
+		$("#errWin").show();        //8.9 
+		$("#errBtn").click (function () {      //8.10 
+				$("#scrnOverlay").remove();    
+				$("#errWin").remove(); 
+			}  ); 
+		   
+		  result = false; 
+	}
 	return result;
 }
 
 /* link HTML elements to corresponding event function */
 function init () {
-	/* assign the <form> element to variable regForm */
-	var regForm = document.getElementById("regform");
-
+	$(".collapse").click(toggle);  //link function toogle() to appropriate events 
 	/* link function validate() to the onsubmit event of the form */
-	regForm.onsubmit = validate;
+	$("#regform").submit(validate);
 }
 
 /* execute function init() once the window is loaded*/
-window.onload = init;
+$(document).ready(init); 
